7002cdac29d9d222c61f99f127e8eb3b770d9043
Utente <Utente@DB841-PC>
2021-01-06 15:58:21 +0100

    Test_classi

--- src/eser_cicli/Asta_01.java
+++ src/eser_cicli/Asta_01.java
@@ -6,6 +6,7 @@ public class Asta_01 {
 
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
+        
         int contendente1, contendente2;
         System.out.print("contendente 1: ");
         contendente1 = inp.nextInt();
